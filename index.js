a5dd628f6e192d919476cfe785f98285d7556abc
Jay Scarrott <a1f6ilr4@students.codeinstitute.net>
2024-12-02 16:49:14 +0000

    add code to js. trying to get buttons to make noise

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#"];
-const notesAudio []
+const notesAudio = [];
 
 function playNote() {
     let sound = Audio(assets/sounds/notes/c4.mp3);
     sound.play()
-    document.getElementById("C-note").addEventListener("click", playNote);
+    document.getElementById("C-note")[0].addEventListener("click", playNote);
 }
