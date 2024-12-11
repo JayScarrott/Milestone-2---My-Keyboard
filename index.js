76a4cca9b803775cdce1ae03ba9d151fc49f0fa9
Jay Scarrott <a1f6ilr4@students.codeinstitute.net>
2024-12-11 20:13:06 +0000

    add object with key-value pairs to JS

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#"];
+const notes = {
+    "C": "assets/sounds/notes/c4.mp3", 
+    "C#": "assets/sounds/notes/c4#.mp3", 
+    "D": "assets/sounds/notes/d5.mp3", 
+    "D#": "assets/sounds/notes/d5#.mp3", 
+    "E": "assets/sounds/notes/e5.mp3", 
+    "F": "assets/sounds/notes/f5.mp3", 
+    "F#": "assets/sounds/notes/f5#.mp3", 
+    "G": "assets/sounds/notes/g5.mp3", 
+    "G#": "assets/sounds/notes/g5#.mp3", 
+    "A": "assets/sounds/notes/a5.mp3", 
+    "A#": "assets/sounds/notes/a5#.mp3", 
+    "B": "assets/sounds/notes/b5.mp3", 
+};
 
 function playNote() {
     let sound = Audio(assets/sounds/notes/c4.mp3);
     sound.play()
-    document.getElementById("C-note")[0].addEventListener("click", playNote);
+    
 }
+
+document.getElementById("C-note")[0].addEventListener("click", playNote);
