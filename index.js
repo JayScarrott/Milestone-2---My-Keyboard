066fd9a0f0213506c89ed953eaf80fe660d5cbac
Jay Scarrott <a1f6ilr4@students.codeinstitute.net>
2024-12-17 11:57:48 +0000

    changed file path names to match new audio files

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 console.log("JavaScript file loaded!");
 
 const notes = {
-    "C-note": "assets/sounds/notes/c4.mp3", 
-    "C-sharp-note": "assets/sounds/notes/c4s.mp3", 
-    "D-note": "assets/sounds/notes/d5.mp3", 
-    "D-sharp-note": "assets/sounds/notes/d5s.mp3", 
-    "E-note": "assets/sounds/notes/e5.mp3", 
-    "F-note": "assets/sounds/notes/f5.mp3", 
-    "F-sharp-note": "assets/sounds/notes/f5s.mp3", 
-    "G-note": "assets/sounds/notes/g5.mp3", 
-    "G-sharp-note": "assets/sounds/notes/g5s.mp3", 
-    "A-note": "assets/sounds/notes/a5.mp3", 
-    "A-sharp-note": "assets/sounds/notes/a5s.mp3", 
-    "B-note": "assets/sounds/notes/b5.mp3", 
+    "C-note": "assets/sounds/notes/c3.mp3", 
+    "C-sharp-note": "assets/sounds/notes/c-3.mp3", 
+    "D-note": "assets/sounds/notes/d3.mp3", 
+    "D-sharp-note": "assets/sounds/notes/d-3.mp3", 
+    "E-note": "assets/sounds/notes/e3.mp3", 
+    "F-note": "assets/sounds/notes/f3.mp3", 
+    "F-sharp-note": "assets/sounds/notes/f-3.mp3", 
+    "G-note": "assets/sounds/notes/g3.mp3", 
+    "G-sharp-note": "assets/sounds/notes/g-3.mp3", 
+    "A-note": "assets/sounds/notes/a3.mp3", 
+    "A-sharp-note": "assets/sounds/notes/a-3.mp3", 
+    "B-note": "assets/sounds/notes/b3.mp3",
+    "C2-note": "assets/sounds/notes/c4.mp3",  
 };
 
 function playNote() {
