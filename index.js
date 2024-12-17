8a3c0b493832d7caadfce8f4500c65bb95609dbe
Jay Scarrott <a1f6ilr4@students.codeinstitute.net>
2024-12-17 11:42:13 +0000

    rename files paths for note sounds.

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ console.log("JavaScript file loaded!");
 
 const notes = {
     "C-note": "assets/sounds/notes/c4.mp3", 
-    "C#-note": "assets/sounds/notes/c4#.mp3", 
+    "C#-note": "assets/sounds/notes/c4s.mp3", 
     "D-note": "assets/sounds/notes/d5.mp3", 
-    "D#-note": "assets/sounds/notes/d5#.mp3", 
+    "D#-note": "assets/sounds/notes/d5s.mp3", 
     "E-note": "assets/sounds/notes/e5.mp3", 
     "F-note": "assets/sounds/notes/f5.mp3", 
-    "F#-note": "assets/sounds/notes/f5#.mp3", 
+    "F#-note": "assets/sounds/notes/f5s.mp3", 
     "G-note": "assets/sounds/notes/g5.mp3", 
-    "G#-note": "assets/sounds/notes/g5#.mp3", 
+    "G#-note": "assets/sounds/notes/g5s.mp3", 
     "A-note": "assets/sounds/notes/a5.mp3", 
-    "A#-note": "assets/sounds/notes/a5#.mp3", 
+    "A#-note": "assets/sounds/notes/a5s.mp3", 
     "B-note": "assets/sounds/notes/b5.mp3", 
 };
 
