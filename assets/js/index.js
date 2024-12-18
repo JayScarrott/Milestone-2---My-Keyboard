39a62a77a62f178968cbcba5bd262c421ebeb2e6
Jay Scarrott <a1f6ilr4@students.codeinstitute.net>
2024-12-18 16:08:59 +0000

    beautify js code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,18 @@
 console.log("JavaScript file loaded!");
 
 const notes = {
-    "C-note": "assets/sounds/notes/a-3.mp3", 
-    "C-sharp-note": "assets/sounds/notes/octave-1/c-3.mp3", 
-    "D-note": "assets/sounds/notes/octave-1/d3.mp3", 
-    "D-sharp-note": "assets/sounds/notes/octave-1/d-3.mp3", 
-    "E-note": "assets/sounds/notes/octave-1/e3.mp3", 
-    "F-note": "assets/sounds/notes/octave-1/f3.mp3", 
-    "F-sharp-note": "assets/sounds/notes/octave-1/f-3.mp3", 
-    "G-note": "assets/sounds/notes/octave-1/g3.mp3", 
-    "G-sharp-note": "assets/sounds/notes/octave-1/g-3.mp3", 
-    "A-note": "assets/sounds/notes/octave-1/a4.mp3", 
-    "A-sharp-note": "assets/sounds/notes/octave-1/a-4.mp3", 
-    "B-note": "assets/sounds/notes/octave-1/b4.mp3", 
+    "C-note": "assets/sounds/notes/a-3.mp3",
+    "C-sharp-note": "assets/sounds/notes/octave-1/c-3.mp3",
+    "D-note": "assets/sounds/notes/octave-1/d3.mp3",
+    "D-sharp-note": "assets/sounds/notes/octave-1/d-3.mp3",
+    "E-note": "assets/sounds/notes/octave-1/e3.mp3",
+    "F-note": "assets/sounds/notes/octave-1/f3.mp3",
+    "F-sharp-note": "assets/sounds/notes/octave-1/f-3.mp3",
+    "G-note": "assets/sounds/notes/octave-1/g3.mp3",
+    "G-sharp-note": "assets/sounds/notes/octave-1/g-3.mp3",
+    "A-note": "assets/sounds/notes/octave-1/a4.mp3",
+    "A-sharp-note": "assets/sounds/notes/octave-1/a-4.mp3",
+    "B-note": "assets/sounds/notes/octave-1/b4.mp3",
 };
 
 /**
@@ -24,11 +24,11 @@ function playNote() {
     const soundPath = notes[this.id];
     const sound = new Audio(soundPath)
     sound.play()
-    
+
 }
 
 const noteButtons = document.querySelectorAll("button");
 
-noteButtons.forEach(function(button) {
+noteButtons.forEach(function (button) {
     button.addEventListener("click", playNote);
 });
