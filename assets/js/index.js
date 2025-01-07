90bd354da9e39265f433b9484d8291176c1bf3bf
Jay Scarrott <a1f6ilr4@students.codeinstitute.net>
2025-01-07 12:05:17 +0000

    add start of sound effect selection code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,27 @@
 console.log("JavaScript file loaded!");
 
-const notes = {
-    "C-note": "assets/sounds/notes/a-3.mp3",
-    "C-sharp-note": "assets/sounds/notes/octave-1/c-3.mp3",
-    "D-note": "assets/sounds/notes/octave-1/d3.mp3",
-    "D-sharp-note": "assets/sounds/notes/octave-1/d-3.mp3",
-    "E-note": "assets/sounds/notes/octave-1/e3.mp3",
-    "F-note": "assets/sounds/notes/octave-1/f3.mp3",
-    "F-sharp-note": "assets/sounds/notes/octave-1/f-3.mp3",
-    "G-note": "assets/sounds/notes/octave-1/g3.mp3",
-    "G-sharp-note": "assets/sounds/notes/octave-1/g-3.mp3",
-    "A-note": "assets/sounds/notes/octave-1/a4.mp3",
-    "A-sharp-note": "assets/sounds/notes/octave-1/a-4.mp3",
-    "B-note": "assets/sounds/notes/octave-1/b4.mp3",
-};
+const soundEffects = {
+    piano: {
+        "C-note": "assets/sounds/notes/a-3.mp3",
+        "C-sharp-note": "assets/sounds/notes/octave-1/c-3.mp3",
+        "D-note": "assets/sounds/notes/octave-1/d3.mp3",
+        "D-sharp-note": "assets/sounds/notes/octave-1/d-3.mp3",
+        "E-note": "assets/sounds/notes/octave-1/e3.mp3",
+        "F-note": "assets/sounds/notes/octave-1/f3.mp3",
+        "F-sharp-note": "assets/sounds/notes/octave-1/f-3.mp3",
+        "G-note": "assets/sounds/notes/octave-1/g3.mp3",
+        "G-sharp-note": "assets/sounds/notes/octave-1/g-3.mp3",
+        "A-note": "assets/sounds/notes/octave-1/a4.mp3",
+        "A-sharp-note": "assets/sounds/notes/octave-1/a-4.mp3",
+        "B-note": "assets/sounds/notes/octave-1/b4.mp3",
+    },
+
+    guitar: {},
+
+    synth: {},
+}
+
+
 
 /**
  * Fucntion for the buttons playing the notes
@@ -33,7 +41,7 @@ noteButtons.forEach(function (button) {
     button.addEventListener("click", playNote);
 });
 
-document.getElementById('submitName').addEventListener('click', function() {
+document.getElementById('submitName').addEventListener('click', function () {
     const userName = document.getElementById('userName').value;
     if (userName) {
         document.getElementById('userGreeting').textContent = `Welcome, ${userName}, to My Keyboard`;
