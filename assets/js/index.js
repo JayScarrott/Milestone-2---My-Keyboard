6f874072895264a99ce44df7c9abc9a9f531e97c
Jay Scarrott <a1f6ilr4@students.codeinstitute.net>
2025-01-07 12:21:52 +0000

    add back notes const, so playnote function still works whilest writing code for sound effect library and funstion

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,6 +21,22 @@ const soundEffects = {
     synth: {},
 }
 
+const notes = {
+    
+        "C-note": "assets/sounds/notes/a-3.mp3",
+        "C-sharp-note": "assets/sounds/notes/octave-1/c-3.mp3",
+        "D-note": "assets/sounds/notes/octave-1/d3.mp3",
+        "D-sharp-note": "assets/sounds/notes/octave-1/d-3.mp3",
+        "E-note": "assets/sounds/notes/octave-1/e3.mp3",
+        "F-note": "assets/sounds/notes/octave-1/f3.mp3",
+        "F-sharp-note": "assets/sounds/notes/octave-1/f-3.mp3",
+        "G-note": "assets/sounds/notes/octave-1/g3.mp3",
+        "G-sharp-note": "assets/sounds/notes/octave-1/g-3.mp3",
+        "A-note": "assets/sounds/notes/octave-1/a4.mp3",
+        "A-sharp-note": "assets/sounds/notes/octave-1/a-4.mp3",
+        "B-note": "assets/sounds/notes/octave-1/b4.mp3",
+};
+
 
 
 /**
